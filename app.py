8cd92fd145447e24ad1874ce50d242c8e4b64539
David Luo <davidkluo@gmail.com>
2023-02-01 10:08:32 -0800

    Fix indentation

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-  os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
+    os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
@@ -21,22 +21,22 @@ toolbar = DebugToolbarExtension(app)
 connect_db(app)
 db.create_all()
 
+
 @app.route("/api/images")
 def get_images():
-  """ grabs all images from the database and returns as json """
+    """ grabs all images from the database and returns as json """
 
-  images = Image.query.order_by(Image.id).all()
+    images = Image.query.order_by(Image.id).all()
 
-  serialized = [image.serialize() for image in images]
+    serialized = [image.serialize() for image in images]
 
-  return jsonify(images = serialized)
+    return jsonify(images=serialized)
 
 
 @app.route("/api/images/<int:id>")
 def get_image(id):
-  """ grabs an image from the database and returns as json """
+    """ grabs an image from the database and returns as json """
 
-  image = Image.query.get_or_404(id)
+    image = Image.query.get_or_404(id)
 
-  return jsonify(image = image.serialized())
-
+    return jsonify(image=image.serialized())
