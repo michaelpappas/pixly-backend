1e26d75805c16bd523b89a212c3a23edee2ac802
David Luo <davidkluo@gmail.com>
2023-01-31 17:19:26 -0800

    Add pseudo code for boto3-aws functions

--- pixly_aws.py
+++ pixly_aws.py
@@ -11,30 +11,42 @@ AWS_ACCESS_KEY_ID=  os.environ['AWS_ACCESS_KEY_ID']
 AWS_REGION = os.environ['AWS_REGION']
 
 
+# def create_presigned_url(expiration=3600):
+#     """Generate a presigned URL to share an S3 object
 
-def create_presigned_url(expiration=3600):
-    """Generate a presigned URL to share an S3 object
+#     :param bucket_name: string
+#     :param object_name: string
+#     :param expiration: Time in seconds for the presigned URL to remain valid
+#     :return: Presigned URL as string. If error, returns None.
+#     """
 
-    :param bucket_name: string
-    :param object_name: string
-    :param expiration: Time in seconds for the presigned URL to remain valid
-    :return: Presigned URL as string. If error, returns None.
-    """
+#     # Generate a presigned URL for the S3 object
 
-    # Generate a presigned URL for the S3 object
+#     s3_client = boto3.client("s3", AWS_REGION,
+#                 aws_access_key_id=AWS_ACCESS_KEY_ID,
+#                 aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+#     try:
+#         response = s3_client.generate_presigned_url('get_object',
+#                                                     Params={'Bucket': AWS_BUCKET_NAME,
+#                                                             'Key': "pixly/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png"},
+#                                                     ExpiresIn=6000)
+#         # response = s3_client.generate_presigned_url('get_object',
+#         #                                             Params={'Bucket': AWS_BUCKET_NAME,
+#         #                                                     'Key': "teamwork.jpeg"},
+#         #                                             ExpiresIn=6000)
+#     except ClientError as e:
+#         logging.error(e)
+#         return None
 
-    s3_client = boto3.client("s3", AWS_REGION,
-                aws_access_key_id=AWS_ACCESS_KEY_ID,
-                aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-    try:
-        response = s3_client.generate_presigned_url('get_object',
-                                                    Params={'Bucket': AWS_BUCKET_NAME,
-                                                            'Key': "teamwork.jpeg"},
-                                                    ExpiresIn=6000)
-    except ClientError as e:
-        logging.error(e)
-        return None
+#     # The response contains the presigned URL
+#     return response
 
-    # The response contains the presigned URL
-    return response
+"""
+- Function to upload an image
+- ? Helper function to construct the thumbnail/original file url
 
+
+Maybe in a separate helper file:
+- Unique filename generator
+
+"""
