7c5b2817c0f1b8431e5475a8c4898c6d025984cc
michaelpappas <pappas.michael@gmail.com>
2023-01-31 16:40:21 -0800

    initial pixly_aws trials

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
 from flask_cors import CORS
+from models import db, connect_db, Image, EXIFData, Tag, ImageTag
 
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+  os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+toolbar = DebugToolbarExtension(app)
 
-@app.route("/")
+# debug = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
+
+@app.route("/api/images")
 def helloWorld():
-  return "Hello, cross-origin-world!"
+  """ grabs all images from the database and returns as json """
+
+  images = [{"image": "image.jpeg", "image_data": "text", "url": "aws/s3/teamwork.jpeg"}]
+  return jsonify(images = images)
---
+++ pixly_aws.py
@@ -0,0 +1,40 @@
+import os
+from dotenv import load_dotenv
+import boto3
+from botocore.exceptions import ClientError
+import logging
+
+load_dotenv()
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_BUCKET_NAME = os.environ['AWS_BUCKET_NAME']
+AWS_ACCESS_KEY_ID=  os.environ['AWS_ACCESS_KEY_ID']
+AWS_REGION = os.environ['AWS_REGION']
+
+
+
+def create_presigned_url(expiration=3600):
+    """Generate a presigned URL to share an S3 object
+
+    :param bucket_name: string
+    :param object_name: string
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Presigned URL as string. If error, returns None.
+    """
+
+    # Generate a presigned URL for the S3 object
+
+    s3_client = boto3.client("s3", AWS_REGION,
+                aws_access_key_id=AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    try:
+        response = s3_client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': AWS_BUCKET_NAME,
+                                                            'Key': "teamwork.jpeg"},
+                                                    ExpiresIn=6000)
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    # The response contains the presigned URL
+    return response
+
