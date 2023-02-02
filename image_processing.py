77022aa44d0e851c44a9733a9dc58b0118d11268
michaelpappas <pappas.michael@gmail.com>
2023-02-02 11:22:55 -0800

    initial gps coordinates processing

--- image_processing.py
+++ image_processing.py
@@ -1,4 +1,6 @@
 from PIL import Image
+from PIL.ExifTags import GPSTAGS
+from lat_lon_parser import to_dec_deg
 
 EXIF_DATA_CODES = {
     'Make': 271,
@@ -18,6 +20,11 @@ def get_exif_data(image_file):
 
     exif_data = img._getexif()
 
+    exposure_calc = exif_data and round(1/exif_data.get(EXIF_DATA_CODES['ExposureTime']))
+    location_data = exif_data and exif_data.get(EXIF_DATA_CODES['GPSInfo'])
+
+    gps_coords = (to_dec_deg(location_data[2]),to_dec_deg(location_data[4]))
+
     image_data_from_file = {
         'width_px': img_width,
         'height_px': img_height,
@@ -25,9 +32,21 @@ def get_exif_data(image_file):
         'device_model': exif_data and exif_data.get(EXIF_DATA_CODES['Model']),
         'focal_length': exif_data and exif_data.get(EXIF_DATA_CODES['FocalLength']),
         'f_stop': exif_data and exif_data.get(EXIF_DATA_CODES['ApertureValue']),
-        'exposure': exif_data and exif_data.get(EXIF_DATA_CODES['ExposureTime']),
-        'location': exif_data and exif_data.get(EXIF_DATA_CODES['GPSInfo']),
+        'exposure': exposure_calc,
+        'location':gps_coords,
         'taken_at': exif_data and exif_data.get(EXIF_DATA_CODES['DateTime'])
     }
 
+
     return image_data_from_file
+
+
+
+    for tag, value in info.items():
+        decoded = TAGS.get(tag, tag)
+        exif_table[decoded] = value
+    gps_info = {}
+    for key in exif_table['GPSInfo'].keys():
+        decode = GPSTAGS.get(key,key)
+        gps_info[decode] = exif_table['GPSInfo'][key]
+    return gps_info
